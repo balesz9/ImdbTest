218b0871b599c426c5d210d59dffa3470262b013
Varga Balint <vargabalint@gmail.com>
2019-04-04 15:31:59 +0200

    feat(main):add signIn and search

--- src/main/java/keywords/Main.java
+++ src/main/java/keywords/Main.java
@@ -7,6 +7,8 @@ public class Main {
     public static void main(String[] args) {
         openIMDB();
         openSignIn();
+        signIn();
+        search();
     }
 
     private static FirefoxDriver driver = new FirefoxDriver();
@@ -20,11 +22,22 @@ public class Main {
         driver.findElementByLinkText("Sign in with IMDb").click();
     }
 
+    public static void signIn(){
+        driver.findElementById("ap_email").sendKeys("rundmcvb17@freemail.hu");
+        driver.findElementById("ap_password").sendKeys("password");
+        driver.findElementById("signInSubmit").click();
+    }
+
+    public static void search(){
+        driver.findElementById("navbar-query").sendKeys("simpsons");
+        driver.findElementById("navbar-submit-button").click();
+    }
+
     public static void closeBrowser(){
         driver.close();
     }
 
-    
+
 
 
 
